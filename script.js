03824dd7115dcb2e66a8d82d2b6df942ed5ae1e5
VICTOR HUGO Nascimento <victorhugovaranda@hotmail.com>
2020-02-10 19:51:57 +0000

    added background saving features

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 let random = document.querySelector(".random");
 let swap = document.querySelector(".swap");
-let dropDown = document.querySelector(".format");
+let dropDown = document.querySelector(".colorFormat");
+let saveButton = document.querySelector(".save");
+let saveList = document.getElementById("saved");
+let fromList = false;
 
 body.style.background = "linear-gradient(to right, "+ color1.value+", "+color2.value+")";
 css.textContent = body.style.background + ";";
 
+saveList.addEventListener("change", setSavedGradient);
+saveButton.addEventListener("click", save);
 dropDown.addEventListener("change", convertCSStext); //only call the function when selecting a different option.
 swap.addEventListener("click", swapColors);
 random.addEventListener("click", randomize);
@@ -23,13 +28,6 @@ function convertCSStext(){
 	css.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")" + ";"
 	:
 	css.textContent = body.style.background + ";"
-
-/*Old function:
-	if (this.value === "hex"){
-		css.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")" + ";";
-	}
-	else if(this.value === "rgb"){
-		css.textContent = body.style.background + ";";	*/
 }
 
 //change background to match color pickers' colours, then update CSS according to if dropDown value is set to rgb or hex.
@@ -40,6 +38,7 @@ function setGradient() {
 	+ ", "
 	+color2.value
 	+")";
+	fromList === true ? fromList = false : saveList.selectedIndex = 0;
 	if (dropDown.value === "rgb"){
 		css.textContent = body.style.background + ";";
 	}
@@ -65,7 +64,6 @@ function randomize(){
 		color2list.push(hexList[select2]);
 		i++;
 	}
-
 	// to convert array to string without being joined by commas, to maintain the hex format. they'll be joined by "" intead. AKA nothing.
 	color1.value = color1list.join("");
 	color2.value = color2list.join("");
@@ -73,9 +71,37 @@ function randomize(){
 	setGradient(color1.value, color2.value);
 }
 
-function swapColors() {
+function swapColors(){
 	let swapColor = color1.value;
 	color1.value = color2.value;
 	color2.value = swapColor;
 	setGradient(color1.value, color2.value);
+}
+
+/*adds a new <option> to the saveList (which is the <select class="saved">).
+this new option's text will contain both color1 and color2 values.
+and if we already have that background saved, do nothing*/
+function save(){
+	let newOption = document.createElement("option");
+	newOption.text = `${color1.value},${color2.value}`;
+	for (var i=0; i<saveList.length; i++){
+		if(newOption.text === saveList.options[i].text){
+			return;
+		}
+	}
+	newOption.style.background = color1.value;
+	saveList.add(newOption);
+}
+
+/*split the values into two values at the comma, ie. #5652cb,#1ad9cb splits into #5652cb and #1ad9cb.
+since this is the correct format, we can pass these as arguments for setGradient.
+fromList is set to true so that the saveList index doesn't change to 'saved backgrounds'
+as it normally would, and instead displays the currently selected saved background.*/
+function setSavedGradient(){
+	let values = event.target.value;
+	let valuesSplit = values.split(',');
+	 color1.value = valuesSplit[0];
+	 color2.value = valuesSplit[1];
+	 fromList = true;
+	setGradient(color1.value, color2.value, fromList);
 }
